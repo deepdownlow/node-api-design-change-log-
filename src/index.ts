c2316e87d7903afcecfa115c8f2fc89e53f7eace
Sina Keshmiri <39638447+deepdownlow@users.noreply.github.com>
2022-12-30 20:12:37 -0800

    fix: resolve typescript request extension

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 import app from './server'
 import * as dotenv from "dotenv";
 import config from './config';
-import * as Express from 'express'
-
-declare global {
-    namespace Express {
-      interface Request {
-        user?: any
-      }
-    }
-  }
 
 dotenv.config()
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any
+    }
+  }
+}
 
 app.listen(config.port, () => {
     console.log(`app is listening. app is listening on port ${config.port}`)
